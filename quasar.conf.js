12b08dfb66d9ac0eded933ef6766aef03b76cc25
Abigayle <achickey@mun.ca>
2024-08-22 11:44:48 +0900

    fix

--- quasar.conf.js
+++ quasar.conf.js
@@ -3,6 +3,7 @@
 module.exports = function (/* ctx */) {
   return {
     build: {
+      vueRouterMode: 'history',
       distDir: 'dist/spa'
     },
     css: {
@@ -18,6 +19,48 @@ module.exports = function (/* ctx */) {
         },
       },
     },
-    // ...
+    ssr: {
+      pwa: false
+    },
+    pwa: {
+      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxOptions: {}, // only for GenerateSW
+      manifest: {
+        name: 'JinStay',
+        short_name: 'JinStay',
+        description: 'JinStay',
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#CD0000',
+        icons: [
+          {
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    },
   }
 }
